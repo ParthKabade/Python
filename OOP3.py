e08130df12ff8d8437da169b95d4a67721f2f6f2
Parth Kabade <151639939+ParthKabade@users.noreply.github.com>
2026-08-17 00:11:07 +0530

    Add files via upload

---
+++ OOP3.py
@@ -0,0 +1,30 @@
+class Demo:
+    #class varaible
+    Value1=10
+    Value2=20
+
+    def __init__(self):
+        self.No1=11
+        self.No2=21
+
+    #Instance method
+    def fun(self):
+        print("Inside instance method name as fun")
+        print(self.No1)
+        print(self.No2)
+        print(Demo.Value1)
+        print(Demo.Value2)
+
+    @classmethod
+    def gun(cls):
+        print("Inside instance method name as gun")
+        print(Demo.No1)
+        print(Demo.No2)
+        print(Demo.Value1)
+        print(Demo.Value2)
+
+dobj = Demo()
+#call without class
+
+dobj.fun()
+dobj.gun()
