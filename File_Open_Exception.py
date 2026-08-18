3e415c72ae411c15d5c529279ad4671a8185161b
Parth Kabade <151639939+ParthKabade@users.noreply.github.com>
2026-08-19 02:08:04 +0530

    Add files via upload

---
+++ File_Open_Exception.py
@@ -0,0 +1,9 @@
+def main():
+    try:
+        open("Demo.txt","r")
+        print("File gets opened")
+    except FileNotFoundError as fobj:
+        print("Files is not present in current directory")
+
+if __name__=="__main__":
+    main()
